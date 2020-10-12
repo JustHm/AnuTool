210d1061fac69ab29bbda481ea73a14e26599e4f
안정흠 <taggma12@gmail.com>
2020-10-12 17:01:44 +0900

    #1 issue commit check

--- app/src/main/java/com/example/anutool/MenuCrawling.java
+++ app/src/main/java/com/example/anutool/MenuCrawling.java
@@ -31,7 +31,6 @@ public class MenuCrawling implements Runnable {
         docs = Jsoup.connect(url)
                 .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21")
                 .timeout(3000)
-                
                 .get();
 
         int statusCode = response.statusCode();
